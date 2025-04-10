c0147537fca1a04a34bc5b69222b394f9a893da7
dhruvathejas <dhruvathejasb@gmail.com>
2025-04-10 16:40:43 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,4 @@
+input.onGesture(Gesture.FreeFall, function () {
+    basic.showString("Loss")
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+})
